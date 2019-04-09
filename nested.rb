5c61ad505302bc80653016753a2618f241d0b36c
Alison Golden <alisong779@yahoo.com>
2019-04-09 19:53:03 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -127,6 +127,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-programmer_hash[:dennis_ritchie][:languages] = ["Assembly"].push
+programmer_hash[:dennis_ritchie][:languages] = ["Assembly"].unshift
 programmer_hash
 end
